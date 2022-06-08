4d05f4517614134efab2c49e754d13f1c5d11af2
Efim Dezhin <efim.dezhin@g42.ai>
2022-06-08 14:25:32 +0400

    MapLibreGL with AntV/L7

---
+++ addons/MapLibre_with_AntV-L7/template/src/Pages/Page2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {MapComponent} from '@/components/MapComponent';
+
+export const Page2 = () => {
+
+  return (
+    <div>
+      <p>Page 2</p>
+      <MapComponent style={{width: '700px', height: '300px'}}/>
+    </div>
+  );
+};
---
+++ addons/MapLibre_with_AntV-L7/template/src/components/MapComponent.tsx
@@ -0,0 +1,96 @@
+import React, {CSSProperties, useEffect, useRef, useState} from 'react';
+import styled from 'styled-components';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import {Scene, LineLayer} from '@antv/l7';
+import {Mapbox} from '@antv/l7-maps';
+import {Map} from 'maplibre-gl';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: stretch;
+  justify-content: stretch;
+`;
+
+const MapElement = styled.div`
+  flex: 0 0 100%;
+`;
+
+interface Props {
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const MapComponent: React.FC<Props> = (props) => {
+  const {
+    className,
+    style
+  } = props;
+  const [map, setMap] = useState<Map | undefined>(undefined);
+  const [scene, setScene] = useState<Scene | undefined>(undefined);
+  const mapElement = useRef<HTMLDivElement>(null);
+  const rootElement = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (mapElement.current) {
+      const m = new Map({
+        container: mapElement.current,
+        style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+        center: [101.94365594271085, 40.46139674355291], // starting position [lng, lat]
+        zoom: 2.5, // starting zoom
+        pitch: 60
+      });
+      setMap(m);
+
+      const s = new Scene({
+        id: m.getContainer() as HTMLDivElement,
+        map: new Mapbox({
+          mapInstance: m
+        })
+      });
+      setScene(s);
+
+      return () => {
+        s.destroy();
+        setMap(undefined);
+        setScene(undefined);
+        rootElement.current!.appendChild(mapElement.current!);
+      };
+    }
+  }, [mapElement]);
+
+  useEffect(() => {
+    if (scene) {
+      scene.on('load', () => {
+        fetch('https://gw.alipayobjects.com/os/rmsportal/UEXQMifxtkQlYfChpPwT.txt')
+          .then(res => res.text())
+          .then(data => {
+            const layer = new LineLayer({})
+              .source(data, {
+                parser: {
+                  type: 'csv',
+                  x: 'lng1',
+                  y: 'lat1',
+                  x1: 'lng2',
+                  y1: 'lat2'
+                }
+              })
+              .size(1)
+              .shape('arc3d')
+              .color('#FF7C6A')
+              .style({
+                opacity: 0.8,
+                sourceColor: '#f00',
+                targetColor: '#6F19FF'
+              });
+            scene.addLayer(layer);
+          });
+      })
+    }
+  }, [scene]);
+
+  return (
+    <Wrapper ref={rootElement} className={className} style={style}>
+      <MapElement ref={mapElement}/>
+    </Wrapper>
+  )
+}
